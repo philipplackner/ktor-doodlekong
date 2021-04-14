cfeffea998bca87189a6e3535a4855d9b01a7477
Philipp <mike.litoris@gmail.com>
2021-04-14 20:28:45 +0200

    Implemented different phases

--- src/data/Room.kt
+++ src/data/Room.kt
@@ -9,6 +9,33 @@ class Room(
     var players: List<Player> = listOf()
 ) {
 
+    private var phaseChangedListener: ((Phase) -> Unit)? = null
+    var phase = Phase.WAITING_FOR_PLAYERS
+        set(value) {
+            synchronized(field) {
+                field = value
+                phaseChangedListener?.let { change ->
+                    change(value)
+                }
+            }
+        }
+
+    private fun setPhaseChangedListener(listener: (Phase) -> Unit) {
+        phaseChangedListener = listener
+    }
+
+    init {
+        setPhaseChangedListener { newPhase ->
+            when(newPhase) {
+                Phase.WAITING_FOR_PLAYERS -> waitingForPlayers()
+                Phase.WAITING_FOR_START -> waitingForStart()
+                Phase.NEW_ROUND -> newRound()
+                Phase.GAME_RUNNING -> gameRunning()
+                Phase.SHOW_WORD -> showWord()
+            }
+        }
+    }
+
     suspend fun broadcast(message: String) {
         players.forEach { player ->
             if(player.socket.isActive) {
@@ -27,5 +54,33 @@ class Room(
 
     fun containsPlayer(username: String): Boolean {
         return players.find { it.username == username } != null
+    }
+
+    private fun waitingForPlayers() {
+
+    }
+
+    private fun waitingForStart() {
+
+    }
+
+    private fun newRound() {
+
+    }
+
+    private fun gameRunning() {
+
+    }
+
+    private fun showWord() {
+
+    }
+
+    enum class Phase {
+        WAITING_FOR_PLAYERS,
+        WAITING_FOR_START,
+        NEW_ROUND,
+        GAME_RUNNING,
+        SHOW_WORD
     }
 }
