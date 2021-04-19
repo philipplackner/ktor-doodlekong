7553e5c2224b0b94a9cfdb351f2b498f1fd8d7e4
Philipp <mike.litoris@gmail.com>
2021-04-19 23:52:20 +0200

    Keeping players alive

--- src/data/Room.kt
+++ src/data/Room.kt
@@ -56,8 +56,30 @@ class Room(
     }
 
     suspend fun addPlayer(clientId: String, username: String, socket: WebSocketSession): Player {
-        val player = Player(username, socket, clientId)
-        players = players + player
+        var indexToAdd = players.size - 1
+        val player = if(leftPlayers.containsKey(clientId)) {
+            val leftPlayer = leftPlayers[clientId]
+            leftPlayer?.first?.let {
+                it.socket = socket
+                it.isDrawing = drawingPlayer?.clientId == clientId
+                indexToAdd = leftPlayer.second
+
+                playerRemoveJobs[clientId]?.cancel()
+                playerRemoveJobs.remove(clientId)
+                leftPlayers.remove(clientId)
+                it
+            } ?: Player(username, socket, clientId)
+        } else {
+            Player(username, socket, clientId)
+        }
+        indexToAdd = when {
+            players.isEmpty() -> 0
+            indexToAdd >= players.size -> players.size - 1
+            else -> indexToAdd
+        }
+        val tmpPlayers = players.toMutableList()
+        tmpPlayers.add(indexToAdd, player)
+        players = tmpPlayers.toList()
 
         if(players.size == 1) {
             phase = Phase.WAITING_FOR_PLAYERS
